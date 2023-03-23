7cc33dabda1f175bb434ba29b4787ee7db828acb
Denis Revin <denis.revin@gmail.com>
2023-03-24 00:01:05 +0400

    add string constants

---
+++ lib/data/overview_strings.dart
@@ -0,0 +1,7 @@
+/// Строковые константы приложения.
+abstract class OverviewStrings {
+  static const String errorMessage =
+      'Ошибка загрузки списка компаний. Проверьте соединение с сетью и попробуйте ещё раз.';
+  static const String name = 'Название';
+  static const String capitalization = 'Капитализация';
+}
