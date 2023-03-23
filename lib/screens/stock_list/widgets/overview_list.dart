afa70f882978dc518ac8987467777796c1813334
Denis Revin <denis.revin@gmail.com>
2023-03-23 20:25:45 +0400

    implement overview list widget

---
+++ lib/screens/stock_list/widgets/overview_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:stock_overview_flutter/data/api/overview_data.dart';
+import 'package:stock_overview_flutter/screens/stock_info/stock_info_screen.dart';
+import 'package:stock_overview_flutter/screens/stock_list/widgets/overview_brief_tile.dart';
+
+class OverviewList extends StatelessWidget {
+  final List<OverviewData> overviewList;
+
+  const OverviewList({
+    required this.overviewList,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: overviewList.length,
+      itemBuilder: (context, index) {
+        final company = overviewList[index];
+        return OverviewBriefTile(
+          name: company.Name ?? '--',
+          capitalization: company.MarketCapitalization ?? '--',
+          onTap: () => _onTap(context, company),
+        );
+      },
+    );
+  }
+
+  void _onTap(BuildContext context, OverviewData company) =>
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (_) => StockInfoScreen(overview: company)),
+      );
+}
