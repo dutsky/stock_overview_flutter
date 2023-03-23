830c733c9ed416c35f5d9856747336db462b46ac
Denis Revin <denis.revin@gmail.com>
2023-03-24 00:30:51 +0400

    implement domain overview model

---
+++ lib/domain/entity/overview.dart
@@ -0,0 +1,98 @@
+/// Доменная модель Overview.
+class Overview {
+  final String symbol;
+  final String assetType;
+  final String name;
+  final String description;
+  final String cik;
+  final String exchange;
+  final String currency;
+  final String country;
+  final String sector;
+  final String industry;
+  final String address;
+  final String fiscalYearEnd;
+  final String latestQuarter;
+  final String marketCapitalization;
+  final String ebitda;
+  final String peRatio;
+  final String pegRatio;
+  final String bookValue;
+  final String dividendPerShare;
+  final String dividendYield;
+  final String eps;
+  final String revenuePerShareTTM;
+  final String profitMargin;
+  final String operatingMarginTTM;
+  final String returnOnAssetsTTM;
+  final String returnOnEquityTTM;
+  final String revenueTTM;
+  final String grossProfitTTM;
+  final String dilutedEPSTTM;
+  final String quarterlyEarningsGrowthYOY;
+  final String quarterlyRevenueGrowthYOY;
+  final String analystTargetPrice;
+  final String trailingPE;
+  final String forwardPE;
+  final String priceToSalesRatioTTM;
+  final String priceToBookRatio;
+  final String evToRevenue;
+  final String evToEbitda;
+  final String beta;
+  final String fiftyTwoWeekHigh;
+  final String fiftyTwoWeekLow;
+  final String fiftyDayMovingAverage;
+  final String twoHundredDayMovingAverage;
+  final String sharesOutstanding;
+  final String dividendDate;
+  final String exDividendDate;
+
+  Overview({
+    required this.symbol,
+    required this.assetType,
+    required this.name,
+    required this.description,
+    required this.cik,
+    required this.exchange,
+    required this.currency,
+    required this.country,
+    required this.sector,
+    required this.industry,
+    required this.address,
+    required this.fiscalYearEnd,
+    required this.latestQuarter,
+    required this.marketCapitalization,
+    required this.ebitda,
+    required this.peRatio,
+    required this.pegRatio,
+    required this.bookValue,
+    required this.dividendPerShare,
+    required this.dividendYield,
+    required this.eps,
+    required this.revenuePerShareTTM,
+    required this.profitMargin,
+    required this.operatingMarginTTM,
+    required this.returnOnAssetsTTM,
+    required this.returnOnEquityTTM,
+    required this.revenueTTM,
+    required this.grossProfitTTM,
+    required this.dilutedEPSTTM,
+    required this.quarterlyEarningsGrowthYOY,
+    required this.quarterlyRevenueGrowthYOY,
+    required this.analystTargetPrice,
+    required this.trailingPE,
+    required this.forwardPE,
+    required this.priceToSalesRatioTTM,
+    required this.priceToBookRatio,
+    required this.evToRevenue,
+    required this.evToEbitda,
+    required this.beta,
+    required this.fiftyTwoWeekHigh,
+    required this.fiftyTwoWeekLow,
+    required this.fiftyDayMovingAverage,
+    required this.twoHundredDayMovingAverage,
+    required this.sharesOutstanding,
+    required this.dividendDate,
+    required this.exDividendDate,
+  });
+}
