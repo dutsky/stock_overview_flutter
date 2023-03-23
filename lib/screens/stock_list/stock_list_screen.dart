c56bc4879186f8561fa543a8970abc78c0f453f6
Denis Revin <denis.revin@gmail.com>
2023-03-23 20:33:28 +0400

    handle future on stock list screen

--- lib/screens/stock_list/stock_list_screen.dart
+++ lib/screens/stock_list/stock_list_screen.dart
@@ -1,20 +1,56 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:stock_overview_flutter/data/api/overview_api.dart';
+import 'package:stock_overview_flutter/data/api/overview_data.dart';
+import 'package:stock_overview_flutter/data/repository/overview_repository.dart';
+import 'package:stock_overview_flutter/screens/stock_list/widgets/overview_list.dart';
 
-class StockListScreen extends StatelessWidget {
+class StockListScreen extends StatefulWidget {
   const StockListScreen({super.key});
+
+  @override
+  State<StockListScreen> createState() => _StockListScreenState();
+}
+
+class _StockListScreenState extends State<StockListScreen> {
+  Future<List<OverviewData>>? overviewList;
+
+  @override
+  void initState() {
+    super.initState();
+    final dio = Dio();
+    final api = OverviewApi(dio);
+    final repository = OverviewRepository(api);
+    overviewList = repository.getOverviewList(['ibm']);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stock Overview List'),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(
+      appBar: AppBar(title: const Text('Stock Overview List')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
           vertical: 20.0,
           horizontal: 16.0,
         ),
-        child: SizedBox.shrink(),
+        child: Center(
+          child: FutureBuilder<List<OverviewData>>(
+            future: overviewList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data != null) {
+                  return OverviewList(overviewList: snapshot.data!);
+                } else {
+                  return const Text('Empty');
+                }
+              } else if (snapshot.hasError) {
+                return const Text('Error');
+              } else {
+                return const CircularProgressIndicator.adaptive();
+              }
+            },
+          ),
+        ),
       ),
     );
   }
